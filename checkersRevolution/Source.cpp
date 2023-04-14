4ca9c2539d86bb5d1fa18e5a8c2d5a989d250751
Misha <bilanm20@gmail.com>
2023-04-14 21:06:21 +0300

    add function print board

--- checkersRevolution/Source.cpp
+++ checkersRevolution/Source.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 using namespace std;
 
+
 const int BOARD_S = 8;
+
 
 const char EMPTY = '-';
 const char O = 'O';
@@ -19,7 +21,32 @@ char board[BOARD_S][BOARD_S] = {
     { O, EMPTY, O, EMPTY, O, EMPTY, O, EMPTY }
 };
 
+void printBoard() {
+    cout << "  ";
+    for (int i = 0; i < BOARD_S; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < BOARD_S; i++) {
+        cout << i << " ";
+        for (int j = 0; j < BOARD_S; j++) {
+            if (board[i][j] != X && EMPTY != board[i][j]) {
+                cout << "\x1b[43m" << board[i][j] << " " << "\033[0m";
+            }
+            else if (board[i][j] == X) {
+                cout << "\x1b[44m" << board[i][j] << " " << "\033[0m";
+            }
+            else {
+                cout << "\x1b[49m" << board[i][j] << " " << "\033[0m";
+            }
+
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
+    printBoard();
     return 0;
 }
