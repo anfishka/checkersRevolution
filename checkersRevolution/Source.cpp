dcdfc5b1ce4a12e2cf220cd2f8fe9e50e98d0261
Hanna Borodina <venera131916@gmail.com>
2023-04-14 21:27:09 +0300

    add function to check validated moves of checkers

--- checkersRevolution/Source.cpp
+++ checkersRevolution/Source.cpp
@@ -2,10 +2,7 @@
 #include <iostream>
 using namespace std;
 
-
 const int BOARD_S = 8;
-
-
 const char EMPTY = '-';
 const char O = 'O';
 const char X = 'X';
@@ -43,6 +40,33 @@ void printBoard() {
         }
         cout << endl;
     }
+}
+
+bool isMoveValid(int startRow, int startCol, int endRow, int endCol, char player) {
+
+    // Check: chacker moves diagonally
+    if (abs(startRow - endRow) != abs(startCol - endCol)) {
+        return false;
+    }
+    // Check : chacker moves only one square or two squares if it captures an opponent's checker 
+    if (abs(startRow - endRow) != 1 && abs(startRow - endRow) != 2) {
+        return false;
+    }
+    // Check: chacker moves to the right or to the left
+    if (abs(startCol - endCol) != 1 && abs(startCol - endCol) != 2) {
+        return false;
+    }
+    // Check: chacker moved and captured opponent's chacker
+    if (abs(startRow - endRow) == 2 && abs(startCol - endCol) == 2) {
+        int enemyRow = (startRow + endRow) / 2;
+        int enemyCol = (startCol + endCol) / 2;
+        if (board[enemyRow][enemyCol] == EMPTY || board[enemyRow][enemyCol] == player) {
+            return false;
+        }
+        board[enemyRow][enemyCol] = EMPTY;
+    }
+    return true;
+
 }
 
 int main()
